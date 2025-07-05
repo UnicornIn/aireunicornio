00f5a67bb36c356b20238c8ef8a999e00ca4dfc5
ENDERBYT3 <endrsanchez.050@gmail.com>
2025-07-05 10:33:22 -0500

    agregue un archivo

---
+++ ejerciciosender/practicas.py
@@ -0,0 +1,5 @@
+nombres = input("introduce tus nombre:" )
+apellidos = input("introduce tus apellidos:")
+edad = int(input("introduce tu edad:" ))
+
+print("hola " + nombres + " " + apellidos + " ,tienes " + str (edad)+ " años.")
