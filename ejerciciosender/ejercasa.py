58aa30e4960928262d43e34a8ebedc4ee07148ec
ENDERBYT3 <endrsanchez.050@gmail.com>
2025-07-12 12:28:36 -0500

    cree un archivo para adivinar un numero

--- ejerciciosender/ejercasa.py
+++ ejerciciosender/ejercasa.py
@@ -1,6 +1,6 @@
-numero =int(input("introduce un numero entero: "))
+#numero =int(input("introduce un numero entero: "))
 
-if numero % 2 == 0:
-    print("El numero es par")
-else:
-    print("El numero es impar")
+#if numero % 2 == 0:
+   # print("El numero es par")
+#else:
+   # print("El numero es impar")
--- ejerciciosender/practicas.py
+++ ejerciciosender/practicas.py
@@ -1,5 +1,5 @@
-nombres = input("introduce tus nombre:" )
-apellidos = input("introduce tus apellidos:")
-edad = int(input("introduce tu edad:" ))
+#nombres = input("introduce tus nombre:" )
+#apellidos = input("introduce tus apellidos:")
+#edad = int(input("introduce tu edad:" ))
 
-print("hola " + nombres + " " + apellidos + " ,tienes " + str (edad)+ " años.")
+#print("hola " + nombres + " " + apellidos + " ,tienes " + str (edad)+ " años.")
